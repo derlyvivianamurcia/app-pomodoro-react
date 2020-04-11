68b98d3ee35acecda97b23821b2b5ce63aa2bc1a
Derly <derlyvivianamurcia@gmail.com>
2020-04-10 19:17:13 -0500

    Pomodoro React

---
+++ src/components/Botones_controladores.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//EVENTO DE START -STOP Y RESET
+export default class Botones_controladores extends Component {
+  render() {
+    return (
+      <div className="controller">
+        <button id="start_stop" onClick={this.props.onStartStop}>
+          {this.props.isStart ? 'Stop' : 'Start'}
+        </button>
+        <button id="reset" onClick={this.props.onReset}>Reset</button>
+      </div>
+    )
+  }
+}
